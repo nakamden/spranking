e3ddf7f41c53c2298cc5f064029526bbe2e70349
nakamden <133067618+nakamden@users.noreply.github.com>
2023-05-10 21:44:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ var svg = d3.select("#plot").append("svg")
     .attr("width", width + padding * 2);
 
 var scaleX = d3.scaleLinear().domain([0, episodes.length - 1]).range([0, width]);
-var scaleY = d3.scaleLinear().domain([0, 30]).range([0, height]);
+var scaleY = d3.scaleLinear().domain([0, 99]).range([0, height * 3]);
 var plot = svg.append("g").attr("transform", "translate(" + padding + "," + padding + ")");
 
 setXAxis();
